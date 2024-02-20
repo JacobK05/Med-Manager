97c174d95f9a56c1376ca157b80586ad3090fe57
Tewodros Assefa <teddyassef23@gmail.com>
2024-02-20 13:44:25 -0600

    Medication page statred

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,11 @@ const routes = require('express').Router();
 // Import the index.js from 'api' folder
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const medicationRoutes = require('./medication-routes');
 
 // When a request is made to the /api route, it will be directed to the index.js in the 'api' folder.
-routes.use('/',homeRoutes);
 routes.use('/api', apiRoutes);
+routes.use('/',homeRoutes);
+routes.use('/medication',medicationRoutes);
 
 module.exports = routes;
